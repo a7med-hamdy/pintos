662d6436512b075cdea97707cf4e232c364bb7db
Mariam Osman <76537563+MariamOsman21@users.noreply.github.com>
2022-05-25 22:03:14 +0200

    some of syscall functions

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -12,9 +12,20 @@
 #include "syscall-nr.h"
 #include "process.h"
 #include "list.h"
+#include <inttypes.h>
 
 static void syscall_handler (struct intr_frame *);
 void validate_pointer(void* p);
+int get_int(int* esp);
+char* get_char_ptr(int* esp);
+void* get_void_ptr(int* esp);
+bool create_wrapper(void* esp);
+bool remove_wrapper(void* esp);
+int wait_wrapper(void* esp);
+unsigned tell_wrapper(void* esp);
+void seek_wrapper(void* esp);
+
+
 struct lock * lock;
 
 struct files{
@@ -57,6 +68,9 @@ syscall_handler (struct intr_frame *f UNUSED)
         // process_execute((char*)*((int*)f->esp+1));
         break;
     }
+    case SYS_WAIT:
+      f->eax = wait_wrapper(f->esp);
+      break;
 
     case SYS_WRITE:
     {
@@ -83,6 +97,22 @@ syscall_handler (struct intr_frame *f UNUSED)
       }
       break;
     }
+    case SYS_CREATE: 
+      f->eax = create_wrapper(f->esp);
+      break;
+    case SYS_REMOVE:
+      f->eax = remove_wrapper(f->esp);
+      break;
+    case SYS_SEEK:
+      seek_wrapper(f->esp);
+      break;
+    case SYS_TELL:
+      f->eax = tell_wrapper(f->esp);
+      break;
+
+
+
+
 
   }
 
@@ -108,3 +138,121 @@ void validate_pointer(void * p)
     process_exit();
   }
 }
+
+
+bool 
+create(const char* file, unsigned initial_size){
+  lock_acquire(&lock);
+  bool is_created = filesys_create(file, initial_size);
+  lock_release(&lock);
+  return is_created;
+}
+
+bool 
+create_wrapper(void* esp){
+  validate_pointer((int*)esp+1);
+  char* file = get_char_ptr((int*)esp+1);
+  validate_pointer((int*)esp +2);
+  unsigned size = get_int((int*)esp+2);
+  return create(file, size);
+}
+
+
+void 
+seek(int fd, unsigned position){
+  //get current thread list of files
+  struct list files = thread_current()->files;
+  struct list_elem* iter = list_begin(&files);
+  //iterate through files list and match fd
+  while(iter != list_end(&files)){
+    struct files* file = list_entry(iter, struct files, elem);
+    if(file->fd == fd){
+       lock_acquire(&lock);
+       file_seek(file->file, position);
+       lock_release(&lock);
+       return;
+    }
+    iter = list_next(iter);
+  }
+}
+
+void 
+seek_wrapper(void* esp){
+  validate_pointer((int*)esp+1);
+  int fd = get_int((int*)esp+1);
+  validate_pointer((int*)esp+2);
+  unsigned position =  get_int((int*)esp+2);
+  seek(fd, position);
+}
+
+unsigned 
+tell(int fd){
+  //get current thread list of files
+  struct list files = thread_current()->files;
+  struct list_elem* iter = list_begin(&files);
+  //iterate through files list and match fd
+  while(iter != list_end(&files)){
+    struct files* file = list_entry(iter, struct files, elem);
+    if(file->fd == fd){
+       lock_acquire(&lock);
+       unsigned position = tell(fd);
+       lock_release(&lock);
+       return fd;
+    }
+    iter = list_next(iter);
+  }
+}
+
+unsigned 
+tell_wrapper(void* esp){
+  validate_pointer((int*)esp + 1);
+  int fd = get_int((int*)esp + 1);
+  return tell(fd);
+}
+
+
+bool 
+remove(const char* file){
+  lock_acquire(&lock);
+  bool is_removed = filesys_remove(file);
+  lock_release(&lock);
+  return is_removed;
+}
+
+
+bool 
+remove_wrapper(void* esp){
+  validate_pointer((int*) esp +1);
+  char* file = get_char_ptr(((int*) esp +1));
+  return remove(file);
+}
+
+
+int
+ wait(tid_t pid){
+  return process_wait(pid);
+}
+int 
+wait_wrapper(void* esp){
+  validate_pointer((int*)esp +1);
+  tid_t pid = get_int((int*)esp +1);
+  return wait(pid);
+
+}
+
+
+int 
+get_int(int* esp){
+  return *esp;
+}
+
+
+char* get_char_ptr(int* esp){
+  return (char*) *esp;
+}
+
+void* get_void_ptr(int* esp){
+  return (void*) *esp;
+}
+
+
